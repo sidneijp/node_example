ec75d814ff0764c0c5692f29eedc90c581a36714
Sidnei Pereira <sidnei@teste.com>
2022-05-10 20:26:47 -0300

    Setup SSL na connection do Sequelize

--- src/db.js
+++ src/db.js
@@ -1,7 +1,14 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://node_example_user:node_example_password@localhost:5432/node_example';
-const db = new Sequelize(DATABASE_URL)
+const db = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
 /*const db = new Sequelize({
     dialect: 'sqlite',
     storage: 'db.sqlite'

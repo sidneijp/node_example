27d1e3e61e30b1bc3e913b19b2e02c8416f10330
Sidnei Pereira <sidnei@teste.com>
2022-05-10 20:06:38 -0300

    String connection Sequelize com Heroku Postgres

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
 
-const db = new Sequelize('postgres://node_example_user:teste@db:5432/node_example')
+const db = new Sequelize('postgres://fumldoswwdttzk:6349291bf5e917025a1834047981d55df89f72aab70107f95db4206972ebde40@ec2-54-164-40-66.compute-1.amazonaws.com:5432/dc3h0jri0bftcu')
 /*const db = new Sequelize({
     dialect: 'sqlite',
     storage: 'db.sqlite'
